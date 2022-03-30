3dcee44c0ee71d13396fe924f29e64938602793a
Carlos Racca <carlosracca1@gmail.com>
2022-03-30 15:42:07 -0300

    prettier and eslint added, also route to / added

--- Routes/index.js
+++ Routes/index.js
@@ -1,13 +1,46 @@
+const axios = require('axios');
 const {Router} = require('express')
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-    try {
-        res.status(200).send('Everything fine')
-    } catch (error) {
-        res.status(404).send('Error getting data')
-    }
+
+    const info = await axios.get('https://localbitcoins.com/sell-bitcoins-online/clp/.json')
+    const infoCleaned = info.data.data.ad_list.map(el =>  el
+        // return{
+        //     id: el.data.ad_id,
+        //     BTC_price_USD: el.data.temp_price_usd,
+        //     BTC_price_CL: el.data.temp_price,
+        //     datetime: el.data.profile.last_online,
+        //     CL_USD: el.data.temp_price / el.data.temp_price_usd
+        // }
+    )
+    
+
+    const infoSorted = infoCleaned.sort((a,b) => {return new Date(b.data.created_at) - new Date(a.data.created_at)})
+    
+    res.status(200).json(infoSorted)
+        
 })
+
+
+router.get('/buy', async (req, res) => {
+ 
+    const info = await axios.get(`https://localbitcoins.com/buy-bitcoins-online/usd/.json`)
+
+    const infoCleaned = info.data.data.ad_list.map(el => {
+        return{
+            id: el.data.ad_id,
+            BTC_price_USD: el.data.temp_price_usd,
+            datetime: el.data.profile.last_online,
+            
+        }
+    })
+
+    const infoSorted = infoCleaned.sort((a,b) => {return b.BTC_price_USD - a.BTC_price_USD})
+    
+    res.status(200).json(infoSorted)
+})
+
 
 module.exports = router
