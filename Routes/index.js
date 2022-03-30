7e573d2a1b382df4a532de87f3cb6108bbc5beb8
Carlos Racca <carlosracca1@gmail.com>
2022-03-30 17:14:53 -0300

    few changes in Routes/index.js

--- Routes/index.js
+++ Routes/index.js
@@ -8,14 +8,14 @@ router.get('/', async (req, res) => {
     const btcToUsdt = await axios.get(`https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT`)
     const UsdToChilean = await axios.get(`https://mindicador.cl/api`)
 
-    const infoCleaned = {
+    const info = {
         BitcoinToUSDT: 'U$S ' + btcToUsdt.data.price,
         UsdToChileanPeso: '$ ' + UsdToChilean.data.dolar.valor,
         BitcoinToChileanPeso: '$ ' + (UsdToChilean.data.dolar.valor - 0) * (btcToUsdt.data.price - 0)
     }
     
     try {
-        res.status(200).json(infoCleaned)
+        res.status(200).json(info)
     } catch (error) {
         res.status(400).send('Cannot get the information')
     }
